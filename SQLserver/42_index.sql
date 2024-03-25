c76a370c85b6246b8ebb22343100c1753c48eec9
mikeethanh <mikeethanh04@gmail.com>
2024-03-25 12:20:48 +0700

    upd

---
+++ SQLserver/42_index.sql
@@ -0,0 +1,31 @@
+﻿--INDEX:
+
+SELECT COUNT(*)
+FROM [Sales].[SalesOrderDetail];
+
+-- Bật hiển thị thống kê về tài nguyên I/O
+SET STATISTICS IO ON;
+
+-- Truy vấn
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE [CarrierTrackingNumber]='1B2B-492F-A9';
+
+-- Bật hiển thị thống kê về tài nguyên I/O
+SET STATISTICS IO OFF;
+
+-- Tạo index cho [CarrierTrackingNumber]
+CREATE INDEX idx_CarrierTrackingNumber 
+ON [Sales].[SalesOrderDetail] ([CarrierTrackingNumber]);
+
+create index idx_AddressLine1
+on Person.Address ([AddressLine1])
+
+-- Bật hiển thị thống kê về tài nguyên I/O
+SET STATISTICS IO ON;
+
+select *
+from Person.Address
+
+-- Bật hiển thị thống kê về tài nguyên I/O
+SET STATISTICS IO OFF;
---
+++ SQLserver/43_view.sql
@@ -0,0 +1,21 @@
+﻿--View (lược đồ) là một bảng ảo được tạo ra từ một hoặc nhiều bảng khác. View không chứa dữ
+--liệu thực tế, mà chỉ là một "lăng kính" để nhìn vào dữ liệu trong các bảng gốc.
+CREATE VIEW MothlySales AS
+SELECT 
+  YEAR([OrderDate]) as "Năm",
+  MONTH([OrderDate]) as "Tháng",
+  COUNT([OrderID]) as "Số lượng đơn hàng"
+FROM
+  [dbo].[Orders]
+GROUP BY YEAR([OrderDate]), MONTH([OrderDate]);
+
+select *
+from MothlySales
+
+
+-- tao view tu bang khach hang va don hang 
+create view CustomerOrders as 
+	select O.OrderID, C.CustomerID
+	from Orders as O
+	join Customers as C
+	on O.CustomerID = C.CustomerID
